cb948f05cc8c8dff689a74d4b67e0e08b5ad5cb3
Cesar Pimentel <cpimentelortiz23@gmail.com>
2026-02-17 12:37:05 -0500

    gradle test

---
+++ src/main/java/com/mcnz/test/gradletest/Launcher.java
@@ -0,0 +1,9 @@
+package com.mcnz.test.gradletest;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
