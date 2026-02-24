7a666725b212bb22d34768b934059f9d8e28ce8f
Roberto Baez <r.baez3672@gmail.com>
2026-02-23 19:00:05 -0500

    feat: create PuzzleApplication class

---
+++ src/PuzzleApplication.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+* PuzzleApplication is the entrypoint class to this project. It loads
+* the first Scene to the Stage provided by the JavaFX runtime.
+* @author Roberto Baez
+*/
+public class PuzzleApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+    /**
+    * Sets the Stage to a Scene with two menu options.
+    * @param stage the primary Stage for the application
+    */
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(PuzzleApplication.class.getResource("menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
