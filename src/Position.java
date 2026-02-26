8e97472d0f4b89917c8b068ef5afadb2ce521b83
Roberto Baez <r.baez3672@gmail.com>
2026-02-25 19:35:58 -0500

    docs: for Position class

--- src/Position.java
+++ src/Position.java
@@ -1,2 +1,14 @@
-// TODO toString()/equals()
+/** Written by: Roberto Baez
+ * Position is a record class which is used
+ * to create immutable objects that represent a
+ * two-dimensional position. It is used to
+ * describe both the location of a Cell within a Grid,
+ * and the location of a Grid within a Board.
+ * @param row     the zero-indexed number representing the
+ *                position's row (vertical axis)
+ * @param col     the zero-indexed number representing the
+ *                position's column (horizontal axis)
+ *
+ * @author Roberto Baez
+ */
 public record Position(int row, int col) { }
